35bdddec3e55e792aadf82659d088cfd5d3b4589
pavl1k999 <zincenkopasa7@gmail.com>
2025-12-28 20:44:57 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -158,7 +158,7 @@ const products = [
   })),
 
   // Cartridge
-  {id: 300, name:'Xros Cartridge 0.6Ω', price: 20, category: 'cartridge', img:'images/cart.jpg'}
+  {id: 300, name:'Xros Cartridge 0.6Ω', price: 20, category: 'cartridge', img:'images/cart.png'}
 ];
 
 // State
